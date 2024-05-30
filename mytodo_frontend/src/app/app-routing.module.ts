f040c5613fffddc74dd53e69909f245ce8a22c89
janie <janie.kabashi@bbzbl-it.ch>
2024-05-30 13:09:41 +0200

    commit changes

--- mytodo_frontend/src/app/app-routing.module.ts
+++ mytodo_frontend/src/app/app-routing.module.ts
@@ -1,10 +1,8 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { appCanActivate } from './guard/app.auth.guard';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { NoAccessComponent } from './pages/no-access/no-access.component';
 import { AppRoles } from '../app.roles';
-
 import { EmployeeListComponent } from './pages/employee-list/employee-list.component';
 import { FolderListComponent } from './pages/folder-list/folder-list.component';
 import { TagListComponent } from './pages/tag-list/tag-list.component';
@@ -13,30 +11,11 @@ import { EmployeeDetailsComponent } from './pages/employee-detail/employee-detai
 import { FolderDetailsComponent } from './pages/folder-detail/folder-details.component';
 import { TagDetailsComponent } from './pages/tag-detail/tag-details.component';
 import { TaskDetailsComponent } from './pages/task-detail/task-details.component';
+import { appCanActivate } from './guard/app.guard';
 
 const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'dashboard', component: DashboardComponent },
-  {
-    path: 'vehicles',
-    component: VehicleListComponent,
-    canActivate: [appCanActivate],
-    data: { roles: [AppRoles.Read] },
-  },
-  {
-    path: 'vehicle',
-    canActivate: [appCanActivate],
-    component: VehicleDetailComponent,
-    pathMatch: 'full',
-    data: { roles: [AppRoles.Admin] },
-  },
-  {
-    path: 'vehicle/:id',
-    canActivate: [appCanActivate],
-    component: VehicleDetailComponent,
-    pathMatch: 'full',
-    data: { roles: [AppRoles.Admin] },
-  },
   {
     path: 'employees',
     component: EmployeeListComponent,
@@ -46,54 +25,74 @@ const routes: Routes = [
   {
     path: 'employee',
     canActivate: [appCanActivate],
-    component: EmployeeDetailComponent,
+    component: EmployeeDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Admin] },
   },
   {
     path: 'employee/:id',
     canActivate: [appCanActivate],
-    component: EmployeeDetailComponent,
+    component: EmployeeDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Admin] },
   },
   {
-    path: 'departments',
-    component: DepartmentListComponent,
+    path: 'folders',
+    component: FolderListComponent,
     canActivate: [appCanActivate],
     data: { roles: [AppRoles.Read] },
   },
   {
-    path: 'department',
+    path: 'folder',
     canActivate: [appCanActivate],
-    component: DepartmentDetailComponent,
+    component: FolderDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Admin] },
   },
   {
-    path: 'department/:id',
+    path: 'folder/:id',
     canActivate: [appCanActivate],
-    component: DepartmentDetailComponent,
+    component: FolderDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Admin] },
   },
   {
-    path: 'vehicle-usages',
-    component: VehicleUsageListComponent,
+    path: 'tags',
+    component: TagListComponent,
     canActivate: [appCanActivate],
     data: { roles: [AppRoles.Read] },
   },
   {
-    path: 'vehicle-usage',
+    path: 'tag',
     canActivate: [appCanActivate],
-    component: VehicleUsageDetailComponent,
+    component: TagDetailsComponent,
+    pathMatch: 'full',
+    data: { roles: [AppRoles.Admin] },
+  },
+  {
+    path: 'tag/:id',
+    canActivate: [appCanActivate],
+    component: TagDetailsComponent,
+    pathMatch: 'full',
+    data: { roles: [AppRoles.Admin] },
+  },
+  {
+    path: 'tasks',
+    component: TaskListComponent,
+    canActivate: [appCanActivate],
+    data: { roles: [AppRoles.Read] },
+  },
+  {
+    path: 'task',
+    canActivate: [appCanActivate],
+    component: TaskDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Update] },
   },
   {
-    path: 'vehicle-usage/:id',
+    path: 'task/:id',
     canActivate: [appCanActivate],
-    component: VehicleUsageDetailComponent,
+    component: TaskDetailsComponent,
     pathMatch: 'full',
     data: { roles: [AppRoles.Update] },
   },
@@ -102,6 +101,6 @@ const routes: Routes = [
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
